85b3a18567f80339d495ddb7301f6e0604e6047a
Umar Hayat <umarhayat9718@gmail.com>
2023-07-26 08:55:09 +0530

    finalising the frame

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -1,5 +1,40 @@
 package main;
 
-public class GamePanel{
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable{
 	
+    //Screen Settings
+    final int originalTileSize = 16; // 16x16 tile size
+    final int scale = 3; // the size should be clear
+
+    public final int tileSize = originalTileSize * scale; // 48x48 are the title size increased 
+    public  final int maxScreenCol = 16; // Number of columns
+    public final int maxScreenRow = 12; // number of rows
+    public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    Thread gameThread;
+
+    //Contructs of Game Panel
+    public GamePanel(){
+
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread () {
+
+        gameThread = new Thread (this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+    }
 }
